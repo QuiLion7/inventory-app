9b5d4316537d5f59836d709d169e9a30c52538c0
QuiLion7 <quilbrub@gmail.com>
2024-11-15 17:16:06 -0300

    feat: add dynamic theme support and render content only on the client

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import Header from "@/components/Header/Header";
+import Table from "@/components/Table/Table";
 import { Card } from "@/components/ui/card";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const { theme } = useTheme();
+  const [isClient, setIsClient] = useState(false);
+  const bgColor = theme === "dark" ? "bg-black" : "bg-gray-50";
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
   return (
-    <div className="p-3">
+    <div className={`poppins p-5 ${bgColor} border w-full min-h-screen`}>
       <Card className="flex flex-col shadow-none p-2">
         <Header />
+        <Table />
       </Card>
     </div>
   );

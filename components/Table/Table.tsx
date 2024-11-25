d0e70739bdb7d4d108e0294cf2dc68fb12f17908
QuiLion7 <quilbrub@gmail.com>
2024-11-24 23:56:11 -0300

    refactor: integrate Zustand state management for product list and add ProductDialog

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,24 +1,34 @@
-import { columns } from "../Product/columns";
-import { products } from "../Product/productData";
-import ProductTable from "../Product/ProductTable";
-import { Button } from "../ui/button";
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import ProductTable from "../Product/ProductTable";
+import { columns } from "../Product/columns";
+import ProductDialog from "../ProductDialog/ProductDialog";
+import { useProductStore } from "@/components/useProductStore";
+import { useEffect } from "react";
 
 export default function Table() {
+  const { allProducts, loadProducts } = useProductStore();
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
   return (
     <Card className="mt-12 flex flex-col shadow-none poppins border-none">
       <CardHeader className="flex justify-between p-2">
         <div className="flex justify-between items-center">
           <div className="">
             <CardTitle className="font-bold text-xl">Produtos</CardTitle>
-            <p className="text-sm text-slate-600">30 produtos</p>
+            <p className="text-sm text-slate-600">
+              {allProducts.length} produtos
+            </p>
           </div>
-          <Button>Add Produto</Button>
+          <ProductDialog />
         </div>
       </CardHeader>
 
       <CardContent>
-        <ProductTable data={products} columns={columns} />
+        <ProductTable data={allProducts} columns={columns} />
       </CardContent>
     </Card>
   );
